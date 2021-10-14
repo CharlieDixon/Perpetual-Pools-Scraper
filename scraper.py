a234e286df6608512f35d7fdd47a1dbfaacdaeef
Charlie Dixon <charlie-dixon@live.co.uk>
2021-10-14 01:12:46 +0100

    selenium workflow successful

--- scraper.py
+++ scraper.py
@@ -33,28 +33,12 @@ def wait_and_click(xpath):
         print(f"exception occured: {e}")
 # click on decline tour for popup on startup
 wait_and_click("/html/body/div[3]/div/div/div/div[2]/div[2]/div[6]/button[1]")
-# try:
-#     popup = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div/div[2]/div[2]/div[6]/button[1]"))
-#     )
-#     popup.click()
-# except Exception as e:
-#     print("exception occured: {e}")
 
 # click dropdown menu for trading pairs
 wait_and_click('//*[@id="headlessui-menu-button-3"]')
-# dropdown = driver.find_element(By.XPATH, '//*[@id="headlessui-menu-button-3"]')
-# driver.execute_script("arguments[0].click();", dropdown)
 
 # wait for dropdown options to appear and click first option
 wait_and_click('//*[@id="headlessui-menu-item-11"]')
-# try:
-#     btc = WebDriverWait(driver, 15).until(
-#         EC.presence_of_element_located((By.XPATH, '//*[@id="headlessui-menu-item-11"]'))
-#     )
-#     btc.click()
-# except Exception as e:
-#     print(f"exception occured: {e}")
 
 # select input
 amount_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[5]/div[1]/input')
@@ -76,34 +60,91 @@ def get_inner_html(xpath):
         logger.debug(e)
 
 token_rate_xpath = '/html/body/div[1]/div/div/div[2]/div/div[6]/div/div/div/div[1]/div[1]/span/div/span[2]'
-token_rate = get_inner_html(token_rate_xpath)
-
 balancer_pools_xpath = '/html/body/div[1]/div/div/div[2]/div/div[6]/div/div/div/div[1]/div[3]/div[2]/a'
-balancer_pools = get_inner_html(balancer_pools_xpath)
 
-percent_diff = ((balancer_pools - token_rate)/token_rate) * 100
-logger.debug(percent_diff)
+test_list = []
 
-power_leverage_3 = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[4]/span/button[2]')
-power_leverage_3.click()
-
+# 1L-BTC/USD
 token_rate = get_inner_html(token_rate_xpath)
 balancer_pools = get_inner_html(balancer_pools_xpath)
-percent_diff = ((balancer_pools - token_rate)/token_rate) * 100
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['1L-BTC/USD'] = token_rate, balancer_pools, percent_diff 
 logger.debug(percent_diff)
 
-# click Short button
-driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[2]/div/div[3]/span[2]/span/button[2]').click()
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[4]/span/button[2]').click() # click leverage 3 button
+
+# 3L-BTC/USD
 token_rate = get_inner_html(token_rate_xpath)
 balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['3L-BTC/USD'] = token_rate, balancer_pools, percent_diff 
+logger.debug(percent_diff)
+
+def get_rates(func):
+    def wrapper(*args, **kwargs):
+        get_inner_html(token_rate_xpath)
+        get_inner_html(balancer_pools_xpath)
+        return func(*args, **kwargs)
+    return wrapper
+
+driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[2]/div/div[3]/span[2]/span/button[2]').click() # click Short button
+
+# 3S-BTC/USD
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['3S-BTC/USD'] = token_rate, balancer_pools, percent_diff 
+
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[4]/span/button[1]').click() # click power leverage button 1
+
+# 1S-BTC/USD
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['1S-BTC/USD'] = token_rate, balancer_pools, percent_diff 
+
+wait_and_click('//*[@id="headlessui-menu-button-3"]') # click on dropdown menu
+wait_and_click('//*[@id="headlessui-menu-item-19"]') # select ETH/USDC pair option
+
+# 1S-ETH/USD
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['1S-ETH/USD'] = token_rate, balancer_pools, percent_diff 
+
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[4]/span/button[2]').click() # click leverage 3 button
+
+# 3S-ETH/USD 
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['3S-ETH/USD'] = token_rate, balancer_pools, percent_diff 
+
+driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[2]/div/div[3]/span[2]/span/button[1]').click() # click Long button
+
+# 3L-ETH/USD
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['3L-ETH/USD'] = token_rate, balancer_pools, percent_diff 
+
+# 1L-ETH/USD
+driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div[4]/span/button[1]').click() # click power leverage 1 button
+token_rate = get_inner_html(token_rate_xpath)
+balancer_pools = get_inner_html(balancer_pools_xpath)
+percent_diff = ((token_rate - balancer_pools)/balancer_pools) * 100
+pairs['1L-ETH/USD'] = token_rate, balancer_pools, percent_diff 
+
+over_one_percent = {k:v for k,v in pairs.items() if pairs[k] >= 1}
 
 breakpoint()
-assert "No results found." not in driver.page_source
 driver.close()
 
 # BTC/USDC dropdown - //*[@id="headlessui-menu-item-11"]
 # ETH/USDC dropdown - //*[@id="headlessui-menu-item-12"]
+# power leverage 1 button - /html/body/div[1]/div/div/div[2]/div/div[4]/span/button[1]
 # power leverage 3 button - /html/body/div[1]/div/div/div[2]/div/div[4]/span/button[2]
 # amount input - /html/body/div[1]/div/div/div[2]/div/div[5]/div[1]/input
+# long button - /html/body/div[1]/div/div/div[2]/div/div[3]/span[2]/span/button[1]
 # short button - /html/body/div[1]/div/div/div[2]/div/div[3]/span[2]/span/button[2]
 # connect wallet button - /html/body/div[1]/div/div/div[2]/div/button
