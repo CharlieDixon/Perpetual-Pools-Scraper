0c6523c263260d9ba89cc6bfedb156c94ed854f8
Charlie Dixon <charlie-dixon@live.co.uk>
2021-10-25 20:08:30 +0100

    file to store current xpaths

---
+++ resources/xpaths.py
@@ -0,0 +1,13 @@
+"""List of xpath constants to be updated in line with website changes"""
+
+SELECT_DROPDOWN = '//*[@id="headlessui-menu-button-3"]'
+BTC_DROPDOWN = '//*[@id="headlessui-menu-item-10"]'
+ETH_DROPDOWN = '//*[@id="headlessui-menu-item-18"]'
+TOKEN_RATE_XPATH = "/html/body/div[1]/div/div/div[3]/div/div[6]/div/div/div/div[1]/div[1]/span/div/span[2]"
+BALANCER_POOLS_XPATH = (
+        "/html/body/div[1]/div/div/div[3]/div/div[6]/div/div/div/div[1]/div[3]/div[2]/a"
+    )
+POWER_LEV_1_XPATH = "/html/body/div[1]/div/div/div[3]/div/div[4]/span/button[1]"
+POWER_LEV_3_XPATH = "/html/body/div[1]/div/div/div[3]/div/div[4]/span/button[2]"
+SHORT_XPATH = "/html/body/div[1]/div/div/div[3]/div/div[3]/span[2]/span/button[2]"
+LONG_XPATH = "/html/body/div[1]/div/div/div[3]/div/div[3]/span[2]/span/button[1]"
